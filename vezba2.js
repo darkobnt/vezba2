22bccc3db612c40262cb44d0533dcb0b926be02e
darkobnt <44034055+darkobnt@users.noreply.github.com>
2018-10-10 21:17:20 +0200

    Add files via upload

--- vezba2.js
+++ vezba2.js
@@ -7,7 +7,12 @@ brojGodJunior=brojDanaJunior/365
 brojDanaExp=(brojSatiExp-brojSatiJunior)/brojSatiUcPosao
 brojDanaUkupno=brojDanaJunior+brojDanaExp
 brojGodUkupno=brojDanaUkupno/365
+brojMeseciExp=(brojDanaUkupno%365)/30
+brojDanaExpOst=(brojDanaUkupno%365)%30
+brojMeseciJun=(brojDanaJunior%365)/30
+brojDanaJunOst=(brojDanaJunior%365)%30
 console.log("broj dana do pocetnika je", brojDanaJunior)
 console.log("broj godina do pocetnika je", brojGodJunior.toFixed(2))
 console.log("broj dana do eksperta je", brojDanaUkupno)
-console.log("broj godina do eksperta je", brojGodUkupno.toFixed(2))
+console.log("vreme do pocetnika je", Math.floor(brojGodJunior),"godina",Math.floor(brojMeseciJun),"meseci",brojDanaJunOst,"dana")
+console.log("vreme do eksperta je", Math.floor(brojGodUkupno),"godina",Math.floor(brojMeseciExp),"meseci",brojDanaExpOst,"dana")
